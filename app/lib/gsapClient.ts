4cb100ab7ee6f16718835e0c0251082cd07df284
SallahBoussettah <boussettahsallah@gmail.com>
2025-10-19 15:31:56 +0100

    Build command fixed

--- app/lib/gsapClient.ts
+++ app/lib/gsapClient.ts
@@ -2,7 +2,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-if (typeof window !== "undefined" && !gsap.core.globals().ScrollTrigger) {
+if (typeof window !== "undefined") {
   gsap.registerPlugin(ScrollTrigger);
 }
 
